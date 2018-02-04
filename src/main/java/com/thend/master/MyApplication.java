5a26c70fad76c2887f27b14f6f854e11c3e92e35
thend <ktshws_yu@163.com>
2018-02-04 09:28:45 +0800

    项目配置tomcat部署

--- src/main/java/com/thend/master/MyApplication.java
+++ src/main/java/com/thend/master/MyApplication.java
@@ -18,7 +18,6 @@ public class MyApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(MyApplication.class);
     }
-
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
     }
